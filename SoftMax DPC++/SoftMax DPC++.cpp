b757a03776ceb2ee8be4e90b96cf1b0588e524cf
Amir <105668409+Sindel7898@users.noreply.github.com>
2024-04-17 01:01:42 +0100

    Added SubGroup Funciton

--- SoftMax DPC++/SoftMax DPC++.cpp
+++ SoftMax DPC++/SoftMax DPC++.cpp
@@ -61,16 +61,19 @@ vector<double> softmax_buffer(const vector<double>& input) {
 
             for (int i = 0; i < NumOfElements; ++i) {
 
-                atomic_ref<double, sycl::memory_order::relaxed, memory_scope::device, access::address_space::global_space> atomicElem(input_accessor[i]);
+                atomic_ref<double, sycl::memory_order::relaxed, memory_scope::device, access::address_space::global_space> InputAtomic(input_accessor[i]);
                 atomic_ref<double, sycl::memory_order::relaxed, memory_scope::device, access::address_space::global_space> atomicdenominator(denominator);
 
-                atomicdenominator += exp(atomicElem.load());
+                atomicdenominator += exp(InputAtomic.load());
             }
 
 
             for (int i = 0; i < NumOfElements; ++i) {
 
-                output_accessor[idx] = exp(input_accessor[idx]) / denominator;
+                atomic_ref<double, sycl::memory_order::relaxed, memory_scope::device, access::address_space::global_space> InputAtomic(input_accessor[idx]);
+                atomic_ref<double, sycl::memory_order::relaxed, memory_scope::device, access::address_space::global_space> OutPutAtomic(output_accessor[idx]);
+
+                OutPutAtomic = exp(InputAtomic.load()) / denominator;
 
             }
 
@@ -96,7 +99,7 @@ vector<double> softmax_USM(const vector<double>& input) {
     const double* input_data = input.data();
     double* output_data = output.data();
 
-    double* sharedArray = malloc_shared<double>(output_data[0], Q);
+    double* sharedArray = malloc_shared<double>(output.size(), Q);
 
 
     Q.submit([&](handler& h) {
@@ -120,6 +123,58 @@ vector<double> softmax_USM(const vector<double>& input) {
 }
 
 
+vector<double> softmax_subgroups(const vector<double>& input) {
+
+    int NumOfElements = input.size();
+
+    vector<double> output(NumOfElements);
+
+    queue Q(cpu_selector_v);
+
+    range<1> num_items(NumOfElements);
+
+    buffer<double, 1> input_buffer(input.data(), num_items);
+    buffer<double, 1> output_buffer(output.data(), num_items);
+    
+    constexpr size_t Size = 9;
+
+    Q.submit([&](handler& h) {
+
+
+        auto input_accessor = input_buffer.get_access<access::mode::read_write>(h);
+        auto output_accessor = output_buffer.get_access<access::mode::write>(h);
+
+        sycl::accessor<float, 1, access::mode::read_write, access::target::local> tileA(range<1>(Size), h);
+        sycl::accessor<float, 1, access::mode::write, access::target::local> tileB(range<1>(Size), h);
+
+        h.parallel_for<class SoftMax>(nd_range<1>(range<1>(NumOfElements), range<1>(Size)), [=](nd_item<1> item) {
+           
+           
+
+            auto local_id = item.get_local_id(0);
+            auto idx = item.get_global_id(0);
+
+            
+            double denominator = 0;
+
+            for (int i = 0; i < NumOfElements; i += NumOfElements) {
+                const int local_idx = local_id + i;
+
+                if (local_idx < NumOfElements) {
+                    denominator += exp(input_accessor[local_idx]);
+                }
+            }
+
+            double local_output = exp(input_accessor[idx]) / denominator;
+
+            output_accessor[idx] = local_output;
+
+            });
+        }).wait();
+
+        return output;
+}
+
 
 int main() {
 
@@ -137,7 +192,7 @@ int main() {
 
     auto start_time = chrono::steady_clock::now();
 
-    vector<double> output = softmax_buffer(input);
+    vector<double> output = softmax_subgroups(input);
 
     auto end_time = chrono::steady_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
